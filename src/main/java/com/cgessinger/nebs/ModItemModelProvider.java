498dec7932e3325a08e1449f4a07edb8e35675b4
CGessinger <christian@gessinger.de>
2021-01-28 10:08:29 +0100

    Remove config

--- src/main/java/com/cgessinger/nebs/ModItemModelProvider.java
+++ src/main/java/com/cgessinger/nebs/ModItemModelProvider.java
@@ -1,23 +1,17 @@
 package com.cgessinger.nebs;
 
-import com.cgessinger.nebs.config.NebsConfig;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import net.minecraft.client.Minecraft;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.resources.IResource;
-import net.minecraft.resources.IResourceManager;
 import net.minecraft.resources.ResourcePackType;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.client.model.generators.ModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
-import sun.net.ResourceManager;
 
 import java.io.*;
 import java.lang.reflect.Type;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
@@ -28,7 +22,7 @@ public class ModItemModelProvider extends ItemModelProvider
 		super(generator, NekosEnchantedBooks.MOD_ID, existingFileHelper);
 	}
 
-	public ModelFile generateModel (String name)
+	public void generateModel (String name)
 	{
 		ResourceLocation location = modLoc("items/" + name);
 		if(!existingFileHelper.exists(location, ResourcePackType.CLIENT_RESOURCES, ".png", "textures"))
@@ -36,27 +30,22 @@ public class ModItemModelProvider extends ItemModelProvider
 			location = mcLoc("item/enchanted_book");
 		}
 
-		return getBuilder(name)
-				.parent(new ModelFile.UncheckedModelFile("item/generated"))
-				.texture("layer0", location);
+		getBuilder(name).parent(new ModelFile.UncheckedModelFile("item/generated")).texture("layer0", location);
 	}
 
 	@Override
 	protected void registerModels ()
 	{
-		System.out.println("register entries model");
-		if(NebsConfig.enchantementMap == null)
+		if(NekosEnchantedBooks.enchantementMap == null)
 		{
 			InputStreamReader input = new InputStreamReader(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("assets/nebs/models/properties.json")), StandardCharsets.UTF_8);
 			Type type = new TypeToken<Map<String, Float>>(){}.getType();
-			NebsConfig.enchantementMap = new Gson().fromJson(new BufferedReader(input), type);
+			NekosEnchantedBooks.enchantementMap = new Gson().fromJson(new BufferedReader(input), type);
 		}
 
-		for(Map.Entry<String, Float> entry : NebsConfig.enchantementMap.entrySet())
+		for(Map.Entry<String, Float> entry : NekosEnchantedBooks.enchantementMap.entrySet())
 		{
-			System.out.println("adding entries model" + entry);
-			ModelFile file = generateModel(entry.getKey().split("\\.")[2]);
-			System.out.println("added model at" + file.getLocation());
+			generateModel(entry.getKey().split("\\.")[2]);
 		}
 	}
 }
--- src/main/java/com/cgessinger/nebs/NekosEnchantedBooks.java
+++ src/main/java/com/cgessinger/nebs/NekosEnchantedBooks.java
@@ -1,6 +1,7 @@
 package com.cgessinger.nebs;
 
-import com.cgessinger.nebs.config.NebsConfig;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.item.ItemModelsProperties;
@@ -8,22 +9,26 @@ import net.minecraft.item.Items;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.Objects;
 
 @Mod(NekosEnchantedBooks.MOD_ID)
 public class NekosEnchantedBooks
 {
 	public static final String MOD_ID = "nebs";
+	public static Map<String, Float> enchantementMap;
 
 	public NekosEnchantedBooks ()
 	{
-		ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, NebsConfig.CLIENT_SPEC, "nebs_config.toml");
-
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
 
 		MinecraftForge.EVENT_BUS.register(this);
@@ -31,12 +36,18 @@ public class NekosEnchantedBooks
 
 	private void doClientStuff (final FMLClientSetupEvent event)
 	{
+		InputStreamReader input = new InputStreamReader(Objects.requireNonNull(
+				GatherDataSubscriber.class.getClassLoader().getResourceAsStream("assets/nebs/models/properties.json")),
+				StandardCharsets.UTF_8);
+		Type type = new TypeToken<Map<String, Float>>(){}.getType();
+		enchantementMap = new Gson().fromJson(new BufferedReader(input), type);
+
 		ItemModelsProperties.registerProperty(Items.ENCHANTED_BOOK, new ResourceLocation("nebs:enchant"), (stack, world, entity) -> {
-			if(NebsConfig.enchantementMap == null)
+			if(enchantementMap == null)
 				return 0.0F;
 
 			String key = EnchantmentHelper.getEnchantments(stack).entrySet().iterator().next().getKey().getName();
-			return NebsConfig.enchantementMap.getOrDefault(key, 0.0F);
+			return enchantementMap.getOrDefault(key, 0.0F);
 		});
 	}
 
@@ -46,7 +57,6 @@ public class NekosEnchantedBooks
 		@SubscribeEvent
 		public static void gatherData (GatherDataEvent event)
 		{
-			System.out.println("gather mod data");
 			DataGenerator gen = event.getGenerator();
 
 			if (event.includeClient())
@@ -54,5 +64,5 @@ public class NekosEnchantedBooks
 				gen.addProvider(new ModItemModelProvider(gen, event.getExistingFileHelper()));
 			}
 		}
-}
+	}
 }
